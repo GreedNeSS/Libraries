ec7b0166a5dde44b0b6912365444256df029d32b
GreedNeSS <ruslan199126@gmail.com>
2022-02-11 20:49:11 +0300

    Добавьте файлы проекта.

---
+++ CarLibrary/DerivedCars.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    public class SportCar : Car
+    {
+        public SportCar()
+        {
+
+        }
+
+        public SportCar(string name, int maxSp, int currSp):
+            base(name, maxSp, currSp)
+        {
+
+        }
+
+        public override void TurboBust()
+        {
+            MessageBox.Show("Ramming speed!", "Fast is better...");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan()
+        {
+
+        }
+
+        public MiniVan(string name, int maxSp, int currSp):
+            base(name, maxSp, currSp)
+        {
+
+        }
+
+        public override void TurboBust()
+        {
+            engState = EngineState.EngineDead;
+            MessageBox.Show("Eek!", "Your engine block exloded!");
+        }
+    }
+}
