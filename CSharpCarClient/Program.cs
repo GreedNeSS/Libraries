91c20f638616214f426b52b5ce612bb3498d4ab1
GreedNeSS <ruslan199126@gmail.com>
2022-02-11 21:10:26 +0300

    Added C# client

---
+++ CSharpCarClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** CarLibrary Client App ***");
+
+            SportCar viper = new SportCar("Viper", 240, 40);
+            viper.TurboBust();
+
+            MiniVan miniVan = new MiniVan();
+            miniVan.TurboBust();
+        }
+    }
+}
